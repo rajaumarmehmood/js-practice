b0562dd1235d09ea9b225508bde1c5f02d3b25df
rajaumarmehmood <rajaumarmehmood2617@gmail.com>
2024-10-10 12:32:34 +0000

    Intro to DataTypes

---
+++ Basics/DataTypes.js
@@ -0,0 +1,16 @@
+let name="Umar"
+let age=20
+let isLoggedIn=true
+console.log(`${name} ${age} ${isLoggedIn}`)
+
+//number 
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol => unique
+
+// object
+console.log(typeof undefined)
+console.log(typeof null)
---
+++ Basics/Variables_let_var_const.js
@@ -0,0 +1,11 @@
+let myfirstname = "Umar";
+let mylastname = "Mehmood";
+var age = 20;
+const cast = "Rajpoot";
+hello = "Hello";
+
+console.log(`${hello} World! From ${myfirstname} ${mylastname}`);
+console.log(`I am ${age} years old`);
+console.log(`My cast is ${cast}`);
+
+// cast="Raja" //cannot change the const variables
